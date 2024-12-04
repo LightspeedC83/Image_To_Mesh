8cfa91a8542a8aeb9d05da600fe0a84bb343cdd8
Chase Lowney <chasea2016@gmail.com>
2024-12-03 21:05:40 -0500

    structure for taking in a jpg file, converting it to black and white, creating pixel objects, doing a flood fill to assign pixel objects to a group based on their group

---
+++ reading_images.py
@@ -0,0 +1,111 @@
+from PIL import Image
+
+#opening the reference image
+reference_name = "test_small.jpg"
+reference_path = f"{reference_name}"
+
+reference_image = Image.open(reference_path)
+reference_pixels = list(reference_image.getdata())
+reference_size = reference_image.size # getting the refence image's size in (x-pixles,y-pixels) format
+
+
+# making a greyscale version of the reference image
+reference = []
+for p in reference_pixels:
+    avg = (p[0]+p[1]+p[2])/3
+    reference.append((avg,avg,avg))
+
+# take the greyscale image and make it purely black and white
+bw_threshold = 50
+bw_reference = []
+for p in reference:
+    if p[0] >= bw_threshold: #if the pixel has value over threshold (ie. pixel is too light), we make it white
+        bw_reference.append((255,255,255))
+    else: #if value is black enough, we make it black
+        bw_reference.append((0,0,0))
+
+
+# creating pixel objects that can also hold other data (we'll hold them in a 2d list)
+class Pixel(): #defining a pixel class
+    def __init__(self, color, id, coordinates):
+        self.color = color
+        self.id = id
+        self.coordinates = coordinates
+        self.x = coordinates[0]
+        self.y = coordinates[1]
+        self.groupID = None
+    
+    def set_groupID(self, groupID):
+        self.groupID = groupID
+
+    def __str__(self):
+        return(f"{self.color} | {self.id} | {self.coordinates}")
+
+pixel_objects = [] # a 2D list that will hold objects of the pixel class, access a pixel via pixel_objects[y][x]
+temp_list = []
+i=0
+for p in bw_reference: #converting all pixel values in bw_reference to Pixel objects and storing them in pixel_objects accordingly
+    if len(temp_list) == reference_size[0]:
+        pixel_objects.append(temp_list)
+        temp_list = []
+    
+    temp_list.append(Pixel(p,i, (i%reference_size[0], len(pixel_objects))))
+
+    i+=1
+pixel_objects.append(temp_list)
+
+
+#defining terminal testing functions
+def print_by_group():
+    for x in pixel_objects:
+        temp = ""
+        for p in x:
+            if p.groupID == None:
+                temp += "0 "
+            else:
+                temp += str(p.groupID+1) + " "
+        print(temp)
+
+def print_by_value():
+    for x in pixel_objects:
+        temp = ""
+        for p in x:
+            if p.color == (255,255,255):
+                temp += ". "
+            else:
+                temp += "# "
+        print(temp)
+
+
+# now we do a flood fill to assign groupIDs to all pixel objects (white pixels have groupID None)
+visited_spots = [[0 for x in range(reference_size[0])]for y in range(reference_size[1])] # a key of visited spots, 0 means we haven't visited, 1 means we have. use this for any given pixel to see if we've visited
+
+group_index = 0
+for y in range(reference_size[1]-1):
+    for x in range(reference_size[0]-1):
+        curr = pixel_objects[y][x]
+
+        if visited_spots[y][x] == 0: # if the pixel under consideration hasn't been visited
+
+            if curr.color == (255,255,255): #if pixel is white we mark as visited and move on
+                visited_spots[y][x] = 1 
+            else:
+                queue = [] #create a queue
+                queue.append(curr) 
+                while len(queue)!=0: #while queue isn't empty we keep looking at neighbors of the queue and assigning them IDs of said group
+                    upNext = queue.pop(0)
+                    upNext.groupID = group_index #assigning the group index for this group
+                    visited_spots[upNext.y][upNext.x] = 1 #marking this pixel as visited
+                    for next_point in [[upNext.x-1, upNext.y], [upNext.x+1, upNext.y], [upNext.x, upNext.y-1], [upNext.x, upNext.y+1]]: 
+                        # print(next_point, visited_spots[next_point[1]][next_point[0]])
+                        try:                          
+                            if visited_spots[next_point[1]][next_point[0]] == 0 and pixel_objects[next_point[1]][next_point[0]].color != (255,255,255): #if the pixel at the potential neighbor position isn't already visited, we add to queue to check
+                                queue.append(pixel_objects[next_point[1]][next_point[0]])  
+                        except: #if there is no pixel at this potential position
+                            pass
+                group_index +=1
+
+
+print_by_group()    
+print()
+print_by_value()
